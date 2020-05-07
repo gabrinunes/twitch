f23e30c433f1b651dd6198856f52afff71116b07
Gabriel Cunha <gn_cunha@hotmail.com>
2020-05-07 13:07:34 -0300

    configurações iniciais

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+const AppStack = createStackNavigator()
+import Home from './Pages/Home'
+export default function Routes(){
+    return(
+      <NavigationContainer>
+         <AppStack.Navigator screenOptions={{headerShown:false}}>
+           <AppStack.Screen name ="Home" component={Home}/>
+         </AppStack.Navigator>
+      </NavigationContainer>
+    )
+}
