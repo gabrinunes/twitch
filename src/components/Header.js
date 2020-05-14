a05b4237662f2eab341ddc3f7d306bd67f4360bb
Gabriel Cunha <gn_cunha@hotmail.com>
2020-05-14 17:45:40 -0300

    iniciando a home com streams

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react'
+import React ,{useState,useEffect} from 'react'
 import {View,Text,TextInput,TouchableOpacity} from 'react-native'
 import styles from './styles'
 import api from '../Services/api'
@@ -7,6 +7,7 @@ import {useDispatch} from 'react-redux'
 export default function Header(){
     const dispatch = useDispatch()
     const [text,setText]=useState('')
+
    async function Search(value){
       dispatch(action.setLoading(true))
       const info = await api.get(`https://api.twitch.tv/kraken/search/streams?query=${value}&limit=30`)
@@ -14,11 +15,19 @@ export default function Header(){
       dispatch(action.setLoading(false))
     }
 
+    async function HomeInit(){
+      const info = await api.get('https://api.twitch.tv/kraken/search/streams?query=overwatch&limit=30')
+      dispatch(action.setStream(info.data.streams))
+    }
+    
+    useEffect(()=>{
+      HomeInit()
+    },[])
     return(
        <View style={styles.header}>
          <View style={{flex:2}}>
            <TextInput
-           placeholder="Search..."
+           placeholder="Search Streams ex:Overwatch"
            onChangeText={(val)=> setText(val)}
            />
          </View>
